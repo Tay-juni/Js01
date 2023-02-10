d8e55b5cb1077a26e80fbfbde75146e3b5b35fd0
cissesalif586@gmail.com <cissesalif586@gmail.com>
2023-02-10 22:03:05 +0100

    j

--- js00/SwapValues.js
+++ js00/SwapValues.js
@@ -2,5 +2,6 @@ var a = 10, b = 20,t;
 t=a
 a=b
 b=t
+
 console.log ("New Value of a:",a);
 console.log("New Value of b:",b);
--- js00/welcomUser.js
+++ js00/welcomUser.js
@@ -1,3 +1,3 @@
  var userNam = 'John';
-
+ 
 console.log('Welcom',userNam);
